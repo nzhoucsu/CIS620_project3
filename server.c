ee62a8262a0ca63273009489541b77f41fcc16eb
nzhoucsu <nzhoucsu@gmail.com>
2018-11-09 11:53:30 -0500

    MODIFY: successfully organize db server hostname, ip and port

--- server.c
+++ server.c
@@ -8,12 +8,6 @@
 #include <unistd.h>
 #include <string.h>
 
-
-/* This program broadcasts a message in datagram to the server and waits
-   for reply. */
-
-#define MSG "CIS 620 is a great course!!!"
-
 #define BUFMAX 2048
 #define UDP_PORT 23997
 
@@ -33,25 +27,19 @@ int main(int argc,char *argv[])
 	struct sockaddr_in db_server_tcp_addr;
 	char db_server_hostname[128];
 	struct hostent *db_server_host;
-	int len = sizeof(db_server_tcp);
+	int len = sizeof(db_server_tcp_addr);
 	// sending message
 	char buf[BUFMAX];
 
-	/* Check if command input meets needs. */
+	// Check if command input meets needs.
 	if (argc != 2)
 	{
 		printf("Please input service-map server name.\n");
 		return -1;
 	}
 
-	/* Get local db server name, IP */
-	// db server gets its own host name.
-	gethostname(db_server_hostname, sizeof(db_server_hostname));
-	// db server gets its own IP address.
-	db_server_host = gethostbyname(db_server_hostname);
-
-	/*	Create TCP server socket for local db server  
-		and get its tcp port which is assigned by system*/
+	// Create TCP server socket for local db server  
+	// and get its tcp port which is assigned by system
 	if ((db_server_tcp_sk = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		printf("db server failed to create tcp socket.\n");
 		return -2;
@@ -68,7 +56,22 @@ int main(int argc,char *argv[])
 	getsockname(db_server_tcp_sk, (struct sockaddr *) &db_server_tcp_addr, &len);
 	db_server_tcp_port = htons(db_server_tcp_addr.sin_port);
 
-	/*	Create UDP client socket for remote service-map server */
+	// Get local db server name, IP
+	// db server gets its own host name.
+	gethostname(db_server_hostname, sizeof(db_server_hostname));
+	// db server gets its own IP address.
+	db_server_host = gethostbyname(db_server_hostname);
+
+	// Organize message sent to service-map server
+	strcpy(buf, db_server_hostname);
+	strcat(buf, ",");
+	tcp_ip_buf = inet_ntoa(*((struct in_addr*) db_server_host->h_addr_list[0]));
+	strcat(buf, tcp_ip_buf);
+	strcat(buf, ",");
+	sprintf(tcp_port_buf, "%d", db_server_tcp_port);
+	strcat(buf, tcp_port_buf);
+
+	// Create UDP client socket for remote service-map server
 	// Get service-map server IP.
 	sev_map_host = gethostbyname(argv[1]);
 	if (sev_map_host == (struct hostent *) NULL){
@@ -82,22 +85,17 @@ int main(int argc,char *argv[])
 	sev_map_udp_addr.sin_port = ntohs(UDP_PORT);
 	// setsockopt is required on Linux, but not on Solaris
 	setsockopt(sev_map_udp_sk,SOL_SOCKET,SO_BROADCAST,(struct sockaddr *)&sev_map_udp_addr,sizeof(sev_map_udp_addr));
+	
+	printf("\nDEBUG--- \n");
+	printf("%s\n", buf);
+	printf("\n");
 
-	/* Create message send to service-map server. */
-	strcpy(buf, db_server_hostname);
-	strcat(buf, ",");
-	tcp_ip_buf = inet_ntoa(*((struct in_addr*) db_server_host->h_addr_list[0]));
-	strcat(buf, tcp_ip_buf);
-	strcat(buf, ",");
-	sprintf(tcp_port_buf, "%d", db_server_tcp_port);
-	// itoa(db_server_tcp_port, tcp_port_buf, 10);	
-	strcat(buf, tcp_port_buf);
+	// // Using UDP, send local db server name, IP and port to remote  service-map server
+	// sendto(sev_map_udp_sk, buf, strlen(buf)+1, 0, (struct sockaddr *)&sev_map_udp_addr, sizeof(sev_map_udp_addr));
 
-	/* Using UDP, send local db server name, IP and port to remote  service-map server */
-	sendto(sev_map_udp_sk, buf, strlen(buf)+1, 0, (struct sockaddr *)&sev_map_udp_addr, sizeof(sev_map_udp_addr));
-
-	/* Using UDP, receive connection confirmation from remote service-map server. */
-	read(sev_map_udp_sk,buf,BUFMAX);
-	printf("%s\n",buf); 
+	// // Using UDP, receive connection confirmation from remote service-map server.
+	// read(sev_map_udp_sk,buf,BUFMAX);
+	// printf("%s\n",buf); 
 	close(sev_map_udp_sk);
+	close(db_server_tcp_sk);
 }
