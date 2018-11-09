e400a9779a8b4cf3eae20216632fcc0c21a3d82d
nzhoucsu <nzhoucsu@gmail.com>
2018-11-09 12:59:18 -0500

    MODIFY: db server can successfully report its host name, IP and port; servic-map server can successfully receive report, store report and send back OK; db server can successfully receive OK

--- server.c
+++ server.c
@@ -91,17 +91,16 @@ int main(int argc,char *argv[])
 	sev_map_udp_addr.sin_port = ntohs(UDP_PORT);
 	// setsockopt is required on Linux, but not on Solaris
 	setsockopt(sev_map_udp_sk,SOL_SOCKET,SO_BROADCAST,(struct sockaddr *)&sev_map_udp_addr,sizeof(sev_map_udp_addr));
-	
+
+	// Using UDP, send local db server name, IP and port to remote  service-map server
+	sendto(sev_map_udp_sk, buf, strlen(buf)+1, 0, (struct sockaddr *)&sev_map_udp_addr, sizeof(sev_map_udp_addr));
 	printf("\nDEBUG--- \n");
-	printf("%s\n", buf);
-	printf("\n");
+	printf("buf sending to service-map: %s\n", buf);
 
-	// // Using UDP, send local db server name, IP and port to remote  service-map server
-	// sendto(sev_map_udp_sk, buf, strlen(buf)+1, 0, (struct sockaddr *)&sev_map_udp_addr, sizeof(sev_map_udp_addr));
-
-	// // Using UDP, receive connection confirmation from remote service-map server.
-	// read(sev_map_udp_sk,buf,BUFMAX);
-	// printf("%s\n",buf); 
+	// Using UDP, receive connection confirmation from remote service-map server.
+	memset(buf, '\0', BUFMAX);
+	read(sev_map_udp_sk,buf,BUFMAX);
+	printf("\nDEBUG---\nbuf receiving from db server: %s\n",buf); 
 	close(sev_map_udp_sk);
 	close(db_server_tcp_sk);
 }
--- servicemap.go
+++ servicemap.go
@@ -8,56 +8,42 @@ import (
     "strings"
 )
  
-table_file := "table_file.txt"
- 
 
 func main() {
-    /* Let's prepare an address at any address at port 10001*/   
+    // Set map table
+    reg_map := make(map[string] string)
+    // Set UDP socket
     ServerAddr,err := net.ResolveUDPAddr("udp",":23997")
     CheckError(err)
- 
-    /* Now listen (misleading in Go);  actually bind at selected port */
     ServerCLNS, err := net.ListenUDP("udp", ServerAddr)
     CheckError(err)
     defer ServerCLNS.Close()
- 
+    // Communicate with db server and client
     buf := make([]byte, 1024)
- 
     for {
         n,addr,err := ServerCLNS.ReadFromUDP(buf)
-        fmt.Println("DEBUG--- Received ",string(buf[0:n]), " from ",addr)
-        s := strings.Split(string(buf[0:n]), ',')
-        if len(s) == 3 // msg from db server
-        {
+        CheckError(err)
+        fmt.Println("DEBUG--- Received ",string(buf[0:n]), "from", addr)
+        s := strings.Split(string(buf[0:n]), ",")
+        if len(s) == 3 { // msg from db server        
             // Display message from db server (name, IP and port)
             fmt.Println("Received from ", s[1], " : PUT ", s[0], " ", s[2])
-            // Write db server name, IP and port into table file
-            write_to_table_file(string(buf[0:n]))
+            // Store db server name, IP and port into table map
+            reg_map["hostname"] = s[0]
+            reg_map["IP"] = s[1]
+            reg_map["port"] = s[2]
+            fmt.Println("DEBUG---", reg_map["hostname"],reg_map["IP"],reg_map["port"]);
             // Reply to db server
             _,err := ServerCLNS.WriteToUDP([]byte("OK"),addr) 
             CheckError(err)
-        }
-        else if strings.EqualFold('client_request', buf[0:n]) // msg from client
-        {
-            // return db server IP and port to client.
-            data_from_table := read_from_table_file()
-            if !strings.EqualFold("no_db_server", data_from_table)
-            {
-                _,err := ServerCLNS.WriteToUDP(,addr) 
-                CheckError(err)
-            }            
-        }
-        else // wrong msg
-        {
+        } else { // wrong msg        
             fmt.Println("Wrong message from db server or client")
             os.Exit(0)
         }
-        
     }
 }
 
 
-/* A Simple function to verify error */
 func CheckError(err error) {
     if err  != nil {
         fmt.Println("Error: " , err)
@@ -65,29 +51,3 @@ func CheckError(err error) {
     }
 }
 
-
-func write_to_table_file(str string){
-    defer file.Close()
-    // Open or create table file.
-    file, err := os.OpenFile(table_file, os.O_CREATE|os.O_WRONLY, 0644)
-    CheckError(err error)
-    // Write db server name, IP and port into table file.
-    _, err = file.WriteString(str)
-    CheckError(err)
-}
-
-
-func read_from_table_file() string{
-    _, err = os.Stat(path)    
-    if os.IsNotExist(err) // create file if not exists
-    {
-        fmt.Println("service-map server hasn't received any db server submission.")
-        return "no_db_server"
-    }
-    file, err := os.OpenFile(table_file, os.O_RDONLY, 0644)
-    CheckError(err)
-    data := make([]byte, 100)
-    count, err := file.Read(data)
-    CheckError(err)
-    return data[:count]
-}
