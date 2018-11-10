576768afd2d54007f24bfeb7272ee4b00aa44d35
nzhoucsu <nzhoucsu@gmail.com>
2018-11-10 08:30:32 -0500

    DEBUG: send ASCII to client

--- servicemap.go
+++ servicemap.go
@@ -6,7 +6,6 @@ import (
     "net"
     "os"
     "strings"
-    "strconv"
 )
  
 
@@ -27,20 +26,17 @@ func main() {
         s := strings.Split(string(buf[0:n]), ",")
         if len(s) == 3 { // msg from db server        
             // Display message from db server (name, IP and port)
-            fmt.Println("Received from ", s[1], " : PUT ", s[0], " ", s[2])
+            fmt.Println("Received from", s[1], ": PUT", s[0], s[2])
             // Store db server name, IP and port into table map
             reg_map["hostname"] = s[0]
-            reg_map["IP"] = s[1]
-            reg_map["port"] = s[2]
+            reg_map["IP_port"]  = strings.Join([]string{s[1],s[2]}, ":")
             // Reply to db server
             _,err := ServerCLNS.WriteToUDP([]byte("OK"),addr) 
             CheckError(err)
         } else if strings.Compare(string(buf[0:n]), string([]byte("client_request"))) == 0 {
-            ss := []string{reg_map["IP"], reg_map["port"]}
-            fmt.Println("enter client_request")
-            fmt.Println(strings.Join(ss, ":"))
-            fmt.Println(strconv.QuoteToASCII(strings.Join(ss, ":")))
-            _,err := ServerCLNS.WriteToUDP([]byte(strings.Join(ss, ":")),addr) 
+            fmt.Println("DEBUG---")
+            fmt.Println("service-map server sends to client:", reg_map["IP_port"])
+            // _,err := ServerCLNS.WriteToUDP(reg_map["IP_port"],addr) 
             CheckError(err)
         } else { // wrong msg        
             fmt.Println("Wrong message from db server or client")
